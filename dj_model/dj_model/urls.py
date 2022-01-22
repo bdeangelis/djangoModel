b4d66c54e689caae972340510a5d029830507377
Brett DeAngelis <brett.deangelis@gmail.com>
2022-01-22 13:58:42 -0500

    adding in drf

--- dj_model/dj_model/urls.py
+++ dj_model/dj_model/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path("", include("hello.urls")),
+    path("", include("hello.urls")),
+    path('api-auth/', include('rest_framework.urls'))
 ]
